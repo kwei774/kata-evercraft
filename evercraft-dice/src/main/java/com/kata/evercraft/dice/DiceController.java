3054d6405b16f46c2d3d5c7bb38a19ba046feb5b
kevin <kevin.t.wei@usps.gov>
2023-05-26 14:56:41 -0400

    random dice

--- evercraft-dice/src/main/java/com/kata/evercraft/dice/DiceController.java
+++ evercraft-dice/src/main/java/com/kata/evercraft/dice/DiceController.java
@@ -1,7 +1,19 @@
 package com.kata.evercraft.dice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.SecureRandom;
+
+@RestController
 public class DiceController {
+    SecureRandom secureRandom;
+
+    public DiceController() {
+        secureRandom = new SecureRandom();
+    }
+    @GetMapping("/roll")
     public Integer roll() {
-        return 0;
+        return secureRandom.nextInt(20);
     }
 }
--- evercraft-dice/src/test/java/com/kata/evercraft/dice/DiceControllerTest.java
+++ evercraft-dice/src/test/java/com/kata/evercraft/dice/DiceControllerTest.java
@@ -1,14 +1,20 @@
 package com.kata.evercraft.dice;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DiceControllerTest {
+
+    DiceController diceController;
+
+    @BeforeEach
+    public void setUp() {
+        diceController = new DiceController();
+    }
     @Test
-    public void shouldReturnNumber() {
-        DiceController diceController = new DiceController();
-        assertThat(diceController.roll()).isEqualTo(0);
+    public void shouldReturnRandomNumberUnder20() {
+        assertThat(diceController.roll()).isLessThan(20);
     }
 }
