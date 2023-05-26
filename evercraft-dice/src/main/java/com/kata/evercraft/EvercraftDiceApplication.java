ca70cfb370b0fe5640dc8686696960376edaabe1
kevin <kevin.t.wei@usps.gov>
2023-05-26 14:15:32 -0400

    submodules

---
+++ evercraft-dice/src/main/java/com/kata/evercraft/EvercraftDiceApplication.java
@@ -0,0 +1,13 @@
+package com.kata.evercraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvercraftDiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvercraftDiceApplication.class, args);
+	}
+
+}
---
+++ evercraft-dice/src/test/java/com/kata/evercraft/EvercraftDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.evercraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvercraftDiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
